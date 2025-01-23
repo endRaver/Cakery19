2a17f8cd59d9ae69bb1d0afbd539bd03b6987c38
Saltypeesh <tungthanh254@gmail.com>
2025-01-23 14:51:11 +0700

    Enhance About Us page and Footer component
    - Updated the Footer component to include a new border for better visual separation.
    - Enhanced the About Us page with a detailed description of the company's story, including a new layout and a "Read More" button linking to the homepage.
    - Added an image to the About Us page for improved visual appeal.

--- frontend/src/layout/components/Footer.tsx
+++ frontend/src/layout/components/Footer.tsx
@@ -4,95 +4,97 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="container relative mx-auto pb-20 pt-9">
-      <div className="flex justify-between gap-8">
-        <ul className="space-y-2 text-sm font-medium leading-6 tracking-wider text-primary-500">
-          <li className="text-nowrap">
-            <Link
-              to={"/"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="text-nowrap">
-            <Link
-              to={"/shop"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              Shop
-            </Link>
-          </li>
-          <li className="text-nowrap">
-            <Link
-              to={"/event"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              Event
-            </Link>
-          </li>
-          <li className="text-nowrap">
-            <Link
-              to={"/about-us"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              About Us
-            </Link>
-          </li>
-          <li className="text-nowrap">
-            <Link
-              to={"/faqs"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              FAQs
-            </Link>
-          </li>
-          <li className="text-nowrap">
-            <Link
-              to={"/contact"}
-              className="border-b border-transparent duration-300 hover:border-primary-500"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+    <div className="border-t border-[#4A1D1F38]">
+      <footer className="container relative mx-auto pb-20 pt-9">
+        <div className="flex justify-between gap-8">
+          <ul className="space-y-2 text-sm font-medium leading-6 tracking-wider text-primary-500">
+            <li className="text-nowrap">
+              <Link
+                to={"/"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="text-nowrap">
+              <Link
+                to={"/shop"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                Shop
+              </Link>
+            </li>
+            <li className="text-nowrap">
+              <Link
+                to={"/event"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                Event
+              </Link>
+            </li>
+            <li className="text-nowrap">
+              <Link
+                to={"/about-us"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                About Us
+              </Link>
+            </li>
+            <li className="text-nowrap">
+              <Link
+                to={"/faqs"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                FAQs
+              </Link>
+            </li>
+            <li className="text-nowrap">
+              <Link
+                to={"/contact"}
+                className="border-b border-transparent duration-300 hover:border-primary-500"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
 
-        <div className="max-w-[245px] space-y-2 text-sm leading-6 tracking-wider text-primary-500">
-          <h4 className="font-medium">Cakery 19</h4>
-          <p>Address: 123 Street Name, State, Switzerland</p>
-          <p>Tel: 0124 456 789</p>
-        </div>
+          <div className="max-w-[245px] space-y-2 text-sm leading-6 tracking-wider text-primary-500">
+            <h4 className="font-medium">Cakery 19</h4>
+            <p>Address: 123 Street Name, State, Switzerland</p>
+            <p>Tel: 0124 456 789</p>
+          </div>
 
-        <div className="space-y-2 text-sm leading-6 tracking-wider text-primary-500">
-          <h4 className="font-medium">Opening Hours</h4>
-          <div className="grid grid-cols-2 gap-x-6 gap-y-2">
-            <p className="text-nowrap">Monday - Friday</p>
-            <p className="text-nowrap">8.00 - 18.30</p>
-            <p className="text-nowrap">Saturday</p>
-            <p className="text-nowrap">Closed</p>
-            <p className="text-nowrap">Sunday</p>
-            <p className="text-nowrap">Closed</p>
+          <div className="space-y-2 text-sm leading-6 tracking-wider text-primary-500">
+            <h4 className="font-medium">Opening Hours</h4>
+            <div className="grid grid-cols-2 gap-x-6 gap-y-2">
+              <p className="text-nowrap">Monday - Friday</p>
+              <p className="text-nowrap">8.00 - 18.30</p>
+              <p className="text-nowrap">Saturday</p>
+              <p className="text-nowrap">Closed</p>
+              <p className="text-nowrap">Sunday</p>
+              <p className="text-nowrap">Closed</p>
+            </div>
+          </div>
+
+          <div className="max-w-[250px] space-y-4 text-justify text-sm leading-6 tracking-wider text-primary-500">
+            <h4 className="font-medium">Newsletter</h4>
+            <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
+            <Input
+              type="email"
+              placeholder="Email"
+              className="rounded-none border-primary-400 bg-transparent text-xs font-medium text-primary-400 outline-none placeholder:text-xs placeholder:text-primary-400 placeholder:opacity-70"
+            />
+
+            <Button className="w-fit rounded-none bg-[#89896E] text-xs">SUBSCRIBE</Button>
           </div>
         </div>
 
-        <div className="max-w-[250px] space-y-4 text-justify text-sm leading-6 tracking-wider text-primary-500">
-          <h4 className="font-medium">Newsletter</h4>
-          <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
-          <Input
-            type="email"
-            placeholder="Email"
-            className="rounded-none border-primary-400 bg-transparent text-xs font-medium text-primary-400 outline-none placeholder:text-xs placeholder:text-primary-400 placeholder:opacity-70"
-          />
-
-          <Button className="w-fit rounded-none bg-[#89896E] text-xs">SUBSCRIBE</Button>
+        <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 flex-col items-center">
+          <img src="/images/cakery_logo_dark.svg" alt="logo" className="w-[100px]" />
+          <div className="mt-2 flex gap-3 text-xs text-primary-200">Website by Cakery</div>
         </div>
-      </div>
-
-      <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 flex-col items-center">
-        <img src="/images/cakery_logo_dark.svg" alt="logo" className="w-[100px]" />
-        <div className="mt-2 flex gap-3 text-xs text-primary-200">Website by Cakery</div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
 
--- frontend/src/pages/aboutUs/AboutUsPage.tsx
+++ frontend/src/pages/aboutUs/AboutUsPage.tsx
@@ -1,8 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const AboutUsPage = () => {
   return (
-    <div>AboutUsPage</div>
-  )
-}
+    <div className="my-10 text-primary-500">
+      <div className="container mx-auto flex gap-20 py-11">
+        <div className="mt-10 flex-1">
+          <h2 className="text-4xl font-medium">About Our Sweet Story</h2>
+          <div className="mt-[18px] border-l-4 border-[#89896E]">
+            <p className="max-w-[466px] ps-10 tracking-wider lg:max-w-[600px]">
+              It is a long established fact that a reader will be distracted by the readable content
+              of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+              more-or-less normal distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English.
+              <br />
+              <br />
+              Many desktop publishing packages and web page editors now use Lorem Ipsum as their
+              default model text, and a search for 'lorem ipsum' will uncover many web sites still
+              in their infancy. Various versions have evolved over the years, sometimes by accident,
+              sometimes on purpose (injected humour and the like).
+            </p>
 
-export default AboutUsPage
+            <Button className="ms-10 mt-6 h-[35px] rounded-[2px] bg-[#89896E] p-1 hover:bg-hover-outline_btn">
+              <Link
+                to="/"
+                className="rounded-[2px] border border-primary-50/40 px-5 py-1.5 text-xs font-medium"
+              >
+                Read More
+              </Link>
+            </Button>
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <img
+            src="/images/about_me.jpg"
+            alt="about_me"
+            className="h-full max-h-[700px] w-full object-cover object-center"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsPage;
