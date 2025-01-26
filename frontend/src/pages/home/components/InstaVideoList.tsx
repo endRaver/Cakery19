6fb06242fbf40293b52c43df1a39cfd9234baf04
Saltypeesh <tungthanh254@gmail.com>
2025-01-27 04:42:39 +0700

    Fix insta video mobile interaction

--- frontend/src/pages/home/components/InstaVideoList.tsx
+++ frontend/src/pages/home/components/InstaVideoList.tsx
@@ -11,6 +11,11 @@ const InstaVideoList = () => {
   const [isHovered, setIsHovered] = useState<number | null>(null);
   const windowWidth = useWindowWidth();
 
+  const handleNavigate = (event: React.MouseEvent, url: string) => {
+    event.stopPropagation();
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="w-full">
       <Carousel
@@ -23,9 +28,9 @@ const InstaVideoList = () => {
           {videos.map((v, index) => (
             <CarouselItem
               key={index}
-              className="flex-shrink-0 basis-auto select-none pl-0 sm:basis-1/3 md:basis-1/4 lg:basis-1/5"
+              className="flex-shrink-0 basis-auto cursor-pointer select-none pl-0 sm:basis-1/3 md:basis-1/4 lg:basis-1/5"
             >
-              <a href={v.url} target="_blank" rel="noopener noreferrer">
+              <div onClick={(event) => handleNavigate(event, v.url)}>
                 <div
                   className="relative"
                   onMouseOver={() => setIsHovered(index)}
@@ -35,7 +40,13 @@ const InstaVideoList = () => {
                     className={`absolute inset-0 flex h-[383px] w-[220px] items-center justify-center bg-gray-800 bg-cover bg-center bg-no-repeat duration-300 sm:h-full sm:w-full ${isHovered !== index ? "" : "opacity-0"}`}
                     style={{ backgroundImage: `url(${v.image})`, zIndex: 10 }}
                   >
-                    <button className="flex size-10 select-none items-center justify-center bg-black/50">
+                    <button
+                      className="flex size-10 select-none items-center justify-center bg-black/50"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsHovered(index);
+                      }}
+                    >
                       <img src={play} alt="play_icon" />
                     </button>
                   </div>
@@ -44,13 +55,13 @@ const InstaVideoList = () => {
                     url={`${v.video}`}
                     width={windowWidth > 640 ? `100%` : `220px`}
                     height={windowWidth > 640 ? `100%` : `383px`}
-                    playing={isHovered === index || windowWidth <= 640}
+                    playing={isHovered === index}
                     controls={false}
                     loop
                     muted
                   />
                 </div>
-              </a>
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
